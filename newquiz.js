63d66072f776dccdb1bcbb8bc30ed252ddb7aded
Akinwumi sehinde <akinwumisehinde@gmail.com>
2022-11-27 15:37:18 +0100

    final edit

--- newquiz.js
+++ newquiz.js
@@ -11,24 +11,25 @@ var questionBank= [
         answer : 'strong'
     },
     {
-        question : 'What is the difference between the <div> and <span> tags?',
-        option : ['div is used where a generic block-level tag is needed, while span is used where a generic inline tag is needed.',
-        'div is used for major divisions on a page, while span is used to span across columns.',
-        'div is the industry-standard default tag, but you could use span if you prefer.',
-        'div is used where a generic inline tag is needed, while span is used where a generic block-level tag is needed.'],
-        answer : 'div is used where a generic block-level tag is needed, while span is used where a generic inline tag is needed.'
+        question : 'Who is the next president of Nigeria?',
+        option : ['Atiku','Sowore',
+        'Peter Obi',
+        'Garri cassava aiiin'],
+        answer : 'Garri cassava aiiin'
     },
     {
-        question : 'Hitler party which came into power in 1933 is known as',
-        option : ['Labour Party','Nazi Party','Ku-Klux-Klan','Democratic Party'],
-        answer : 'Nazi Party'
+        question : 'Due to the current increase in Breakfast served by women, How many times shouldyou fear women perday?',
+        option : ['44 times','2hrs','Never fear women','Turn gay'],
+        answer : 'Never fear women',
     },
     {
-        question : 'First human heart transplant operation conducted by Dr. Christiaan Barnard on Louis Washkansky, was conducted in',
-        option : ['1967','1968','1958','1922'],
-        answer : '1967'
+        question : 'Who will win this year"s world cup?',
+        option : ['Brazil','France','Ghana','Spain'],
+        answer : 'Ghana'
     }
+   
 ]
+
 
 var question= document.getElementById('question');
 var quizContainer= document.getElementById('quiz-container');
@@ -42,21 +43,29 @@ var points= document.getElementById('score');
 var span= document.querySelectorAll('span');
 var i=0;
 var score= 0;
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
+let timeValue =  15;
+// var scoreboard = document.getElementById('scoreboard')
 
 // //function to display questions
 function displayQuestion(){
-    for(var a=0 ;a<span.length;a++){
+    for(var a=0;a<span.length;a++){
         span[a].style.background='none';
     }
+    
     question.innerHTML= 'Q.'+(i+1)+' '+questionBank[i].question;
     option0.innerHTML= questionBank[i].option[0];
     option1.innerHTML= questionBank[i].option[1];
     option2.innerHTML= questionBank[i].option[2];
     option3.innerHTML= questionBank[i].option[3];
     stat.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
+    startTimer(15); //calling startTimer function
+    startTimerLine(0); //calling startTimerLine function
 }
 
-// //function to calculate scores
+// function to calculate scores
 function calcScore(e){
     if(e.innerHTML===questionBank[i].answer && score<questionBank.length)
     {
@@ -69,6 +78,7 @@ function calcScore(e){
     setTimeout(nextQuestion,300);
 }
 
+
 // function to display next question
 function nextQuestion(){
     if(i<questionBank.length-1)
@@ -79,31 +89,63 @@ function nextQuestion(){
     else{
         points.innerHTML= score+ '/'+ questionBank.length;
         quizContainer.style.display= 'none';
-        scoreboard.style.display= 'block'
+        scoreboard.style.display= 'block';
     }
 }
 
 //click events to next button
 next.addEventListener('click',nextQuestion);
 
-//Back to Quiz button event
+// Back to Quiz button event
 function backToQuiz(){
     location.reload();
 }
 
+
 // function to check Answers
-function checkAnswer(){
-    var answerBank= document.getElementById('answerBank');
-    var answers= document.getElementById('answers');
-    answerBank.style.display= 'block';
-    scoreboard.style.display= 'none';
-    for(var a=0;a<questionBank.length;a++)
-    {
-        var list= document.createElement('li');
-        list.innerHTML= questionBank[a].answer;
-        answers.appendChild(list);
-    }
-}
+// function checkAnswer(){
+//     var answerBank= document.getElementById('answerBank');
+//     var answers= document.getElementById('answers');
+//     answerBank.style.display= 'block';
+//     scoreboard.style.display= 'none';
+//     for(var a=0;a<questionBank.length;a++)
+//     {
+//         var list= document.createElement('li');
+//         list.innerHTML= questionBank[a].answer;
+//         answers.appendChild(list);
+//     }
+// }
+
+
+
+// timer
+// function startTimer(time){
+//     counter = setInterval(timer, 1000);
+//     function timer(){
+//         timeCount.textContent = time; //changing the value of timeCount with time value
+//         time--; //decrement the time value
+//         if(time < 9){ //if timer is less than 9
+//             let addZero = timeCount.textContent; 
+//             timeCount.textContent = "0" + addZero; //add a 0 before time value
+//         }
+//         if(time < 0){ //if timer is less than 0
+//             clearInterval(counter); //clear counter
+//             timeText.textContent = "Time Off"; //change the time text to time off
+            
+//         }
+//     }
+// }
+
+// function startTimerLine(time){
+//     counterLine = setInterval(timer, 29);
+//     function timer(){
+//         time += 1; //upgrading time value with 1
+//         time_line.style.width = time + "px"; //increasing width of time_line with px by time value
+//         if(time > 549){ //if time value is greater than 549
+//             clearInterval(counterLine); //clear counterLine
+//         }
+//     }
+// }
 
 
 displayQuestion();
